6dfadff73bd9ecd3a1b1be1de30aef205f07ed32
Amrit Twanabasu <amrit.cas@gmail.com>
2019-03-20 18:53:35 +0545

    Setup Entry Point and Basic Application

---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Core.h"
+
+namespace Hazel {
+
+	class HAZEL_API Application
+	{
+	public:
+		Application();
+		virtual ~Application();
+
+		void Run();
+	};
+
+	// Should be defined in the client (Sandbox).
+	Application* CreateApplication();
+}
